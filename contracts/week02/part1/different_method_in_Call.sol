3b5a3d78097efce012f8c7c810768d2f91df40f1
xiaotong <83378122@163.com>
2025-04-26 15:29:40 +0800

    call

--- contracts/week02/part1/different_method_in_Call.sol
+++ contracts/week02/part1/different_method_in_Call.sol
@@ -24,18 +24,26 @@ contract Caller{
 
     
     function inc() external {
-
+       (bool _r,bytes memory data)= to.call(abi.encodeWithSignature("inc()"));
+       require(_r,"execute fail");
+       console.logBytes(data);
     }
 
 
     function setN(uint256 _number) external {
-
+      (bool _r,bytes memory data)= to.call(abi.encodeWithSignature("setN(uint256)",_number));
+       require(_r,"execute fail");
+       console.logBytes(data);
     }
 
     function delegateSetN(uint256 _number) external {
-
+      (bool _r,bytes memory data)= to.delegatecall(abi.encodeWithSignature("setN(uint256)",_number));
+       require(_r,"execute fail");
+       console.logBytes(data);
     }
      function staticSetN() view external {
-
+      (bool _r,bytes memory data)= to.staticcall(abi.encodeWithSignature("number()"));
+       require(_r,"execute fail");
+       console.logBytes(data);
     }
 }
